4e2f31cb569109c7d367e9a4f3ca3987bb41a5b7
Alli K <kellermanalli@gmail.com>
2022-07-13 12:13:47 -0500

    Pushing assignment to LC git repo

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,7 +3,7 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    const missionTarget = document.getElementById("missionTarget");
-//    console.log(missionTarget);
+
    missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
